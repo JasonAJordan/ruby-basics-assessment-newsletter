b64971b671842a48744dc50af1af7e6332c63bc6
JasonAkiraJordan <jasonajordan0@gmail.com>
2020-10-26 16:57:38 -0400

    Jason's Work

--- newsletter.rb
+++ newsletter.rb
@@ -30,31 +30,61 @@ ARTICLES = [
 def calculate_recipients
   # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
   # write a method that will return an array of only the subscribers who haven't unsubscribed
+  newArray = []
+  counter = 0
+  
+
+  while counter < SUBSCRIBERS.length
+  
+    if UNSUBSCRIBED.index(SUBSCRIBERS[counter]) == nil
+      newArray.push(SUBSCRIBERS[counter])
+    end 
+    
+  counter += 1
+  end 
+  return newArray
+
 end
 
-def first_n_articles(number_of_articles
-  ARTICLES.first(number_of_articles)
+def first_n_articles(number_of_articles)
+  ARTICLES.first(number_of_articles.to_f)
 end
 
 def print_recipients
   # Write a method that uses the output of calculate_recipients
   # and returns a list of emails separated by commas
   # Ex) "abc@email.com, def@email.com, ghi@email.com"
+
+  array = calculate_recipients
+
+  list = array.join(", ")
+  puts list
+
 end
 
 def print_one_article(article)
   # Write a method that will take an article hash
   # and print the title, author and text as a formatted string
   # See the README/sample output for examples
+
+  puts "#{article[:title]} \nby: #{article[:author]} \n#{article[:text]}\n "
 end
 
 def print_many_articles(articles)
   # Write a method that will take in an array of article hashes
   # and format each one using the print_one_article method
+  count = 0
+
+  while count < articles.length
+    print_one_article(articles[count])
+  count += 1
+  end 
+
 end
 
 def format_campus_location(campus)
-  "Flatiron #{campus["name"]}"
+  name = campus[:name]
+ return "Flatiron #{name}"
 end
 
 def format_subject
@@ -80,7 +110,7 @@ def print_newsletter(number)
   print_many_articles(articles)
   puts format_footer(CAMPUS)
 
-  end
+  
 end
 
 def run
